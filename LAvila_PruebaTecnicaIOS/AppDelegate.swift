53959f331c99ea9ad8694fa4128481e313b58f1e
LAAvilaVazquez21 <lavilavazquez5@gmail.com>
2023-10-11 15:25:46 -0600

    PruebaTecnicaIOS

--- LAvila_PruebaTecnicaIOS/AppDelegate.swift
+++ LAvila_PruebaTecnicaIOS/AppDelegate.swift
@@ -1,6 +1,7 @@
 
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -9,6 +10,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        
+        IQKeyboardManager.shared.overrideKeyboardAppearance = true
+            //IQKeyboardManager.shared.keyboardAppearance = .dark
+        
+        IQKeyboardManager.shared.keyboardDistanceFromTextField = 60.0
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
+        
         return true
     }
 
